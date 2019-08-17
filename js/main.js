b2db354777b5d91d003e7698522abcccc899fa5e
Suherman <suherman@qantas.com.au>
2019-08-17 11:48:39 +1000

    Add multi language support.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,45 @@
 new WOW().init();
+
 $(function () {
     var includes = $('[data-include]');
     jQuery.each(includes, function () {
-        var file = 'view/' + $(this).data('include') + '.html';
-        $(this).load(file);
+        var include = $(this).data('include');
+        var lang = getUrlVars().lang;
+        var excludes = ['footer', 'nav-bar', 'scripts', 'styles'];
+        var suffix = !excludes.includes(include) ? (lang ? '-' + lang : '-en') : '';
+        var file = 'view/' + $(this).data('include') + suffix + '.html';
+        $(this).load(file, function () {
+
+            var lang = getUrlVars().lang;
+            if (lang) {
+                $('a').each(function () {
+                    var href = $(this).attr('href');
+                    if (href) {
+                        var anchor = href.match('#') ? '#' + href.slice(href.indexOf('#') + 1) : null;
+                        var main = href.match(/#/g) ? href.slice(0, href.indexOf('#')) : href;
+                        if (!main.match(/lang/g)) {
+                            main += '?lang=' + lang;
+                        }
+                        console.log(`${href} - ${main} - ${anchor}`);
+                        var final = (anchor && anchor !== '/') ? (main + anchor) : main;
+                        $(this).attr('href', final);
+                    }
+                });
+            }
+        });
     });
 });
+
+
+
+function getUrlVars() {
+    var vars = [],
+        hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
