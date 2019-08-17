156163ca3a2742de407042b9e10daffc4f685e81
Suherman <suherman@qantas.com.au>
2019-08-18 07:16:09 +1000

    Update scripts using ES6, replace html import with javascript, import css in side main.css.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,12 @@
-setTimeout(() => { new WOW().init(); }, 1200) 
+const appendScript = (src) => {
+    const script = document.createElement('script');
+    script.src = src;
+    script.type = 'text/javascript';
+    document.head.appendChild(script);
+};
 
-$(function () {
-    var includes = $('[data-include]');
-    jQuery.each(includes, function () {
-        var include = $(this).data('include');
-        var lang = getUrlVars().lang;
-        var excludes = ['footer', 'nav-bar', 'scripts', 'styles'];
-        var suffix = !excludes.includes(include) ? (lang ? '-' + lang : '-en') : '';
-        var file = 'view/' + $(this).data('include') + suffix + '.html';
-        $(this).load(file, function () {
-
-            var lang = getUrlVars().lang;
-            if (lang) {
-                $('a').each(function () {
-                    var href = $(this).attr('href');
-                    if (href) {
-                        var anchor = href.match('#') ? '#' + href.slice(href.indexOf('#') + 1) : null;
-                        var main = href.match(/#/g) ? href.slice(0, href.indexOf('#')) : href;
-                        if (!main.match(/lang/g)) {
-                            main += '?lang=' + lang;
-                        }
-                        var final = (anchor && anchor !== '/') ? (main + anchor) : main;
-                        $(this).attr('href', final);
-                    }
-                });
-            }
-        });
-    });
-});
-
-
-
-function getUrlVars() {
-    var vars = [],
+const getUrlVars = () => {
+    let vars = [],
         hash;
     var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
     for (var i = 0; i < hashes.length; i++) {
@@ -41,4 +15,55 @@ function getUrlVars() {
         vars[hash[0]] = hash[1];
     }
     return vars;
-}
+};
+
+(function () {
+    var startingTime = new Date().getTime();
+    appendScript('/js/jquery-3.2.1.min.js');
+    appendScript('/js/popper.min.js');
+    appendScript('/js/bootstrap.min.js');
+    appendScript('/js/mdb.js');
+
+    var checkReady = function (callback) {
+        if (window.jQuery) {
+            callback(jQuery);
+        } else {
+            window.setTimeout(function () {
+                checkReady(callback);
+            }, 20);
+        }
+    };
+
+    checkReady(function ($) {
+        $(function () {
+            setTimeout(() => {
+                new WOW().init();
+            }, 1200);
+
+            $(() => {
+                const includes = $('[data-include]'),
+                    lang = getUrlVars().lang,
+                    excludes = ['footer', 'navbar', 'scripts', 'styles'];
+
+                jQuery.each(includes, function () {
+                    const include = $(this).data('include'),
+                        suffix = !excludes.includes(include) ? (lang ? '-' + lang : '-en') : '';
+
+                    $(this).load('view/' + include + suffix + '.html', () => {
+                        if (lang) {
+                            $('a').each(function () {
+                                const href = $(this).attr('href');
+                                if (href) {
+                                    const anchor = href.match('#') ? '#' + href.slice(href.indexOf('#') + 1) : null;
+                                    let main = href.match(/#/g) ? href.slice(0, href.indexOf('#')) : href;
+                                    main += (!main.match(/lang/g)) ? '?lang=' + lang : '';
+                                    $(this).attr('href', (anchor && anchor !== '/') ? (main + anchor) : main);
+                                }
+                            });
+                        }
+                    });
+                });
+            });
+        });
+    });
+})();
